ab200737bf6dd40f73a55c6103e85c07568fcc3e
WarpWing <professornitro@gmail.com>
2021-08-09 17:11:10 -0400

    Baseline

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Reaper
-A system service that assists with daily system operations 
+My personalized AI System made in Python. This currently only works on MacOS / Linux. 
--- src/reaper.py
+++ src/reaper.py
@@ -1,30 +1,86 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import time
+import subprocess
 import os 
 import sys
-import yaml 
+import json
 import requests
 import platform 
 import socket,json,psutil,logging
 
-class sysinfo: 
-    uname = platform.uname()
-    release = uname.system
-    kernel = uname.release
-    version = uname.version
-    machine = uname.machine
-    name = uname.node
-    ram= str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
 
-    
-## This code is for debugging and logging. This will not be externally collected but will be stored at the beginning of every log message
-#uname = platform.uname()
-#base = {}
-#base['system']=uname.system
-#base['release']=uname.release
-#base['version']=uname.version
-#base['machine']=uname.machine
-#base['nodename']=uname.node
-#base['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
-#json.dumps(base)
+
+
+
+
+# Voice Engine Init
+engine=pyttsx3.init('espeak')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0]id')
+
+
+#Small TTS Function
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+#Greet Function 
+def greeting():
+    hour=datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as src:
+        print("Listening for commands")
+        audio=r.listen(src)
+
+        try:
+            input=r.recognize_google(audio,language='en-in')
+            print(f"User said:{input}\n")
+
+        except Exception as e:
+            speak("Apologies, please speak up")
+            return "None"
+        return input
+
+speak("Loading up Reaper")
+greeting()
+
+
+if __name__=='__main__':
+
+
+    while True:
+        speak("Awaiting command.")
+        statement = takeCommand().lower()
+        if statement==0: #This begins the command array
+            continue
+
+        if "goodbye" in statement or "terminate" in statement:
+            speak('Shutting down Reaper')
+            break
+
+        elif 'test' in statement:
+            speak("Test command")
+            time.sleep(2)
+           
+            
+
+
+
+time.sleep(2)
+
 
 
 
